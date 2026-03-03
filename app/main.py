533f4da6d40ab3ed6499680783e29ff0787fc757
laye-samb <151043974+laye-samb@users.noreply.github.com>
2026-03-03 18:25:19 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,23 +1,5 @@
-from fastapi import FastAPI
-import xml.etree.ElementTree as ET
+def main():
+    print("Application Python démarrée")
 
-app = FastAPI()
-
-@app.get("/")
-def home():
-    return {"message": "Application Python démarrée"}
-
-@app.get("/employees")
-def get_employees():
-    tree = ET.parse("employ.xml")
-    root = tree.getroot()
-
-    employees = []
-    for emp in root.findall("employee"):
-        employees.append({
-            "id": emp.find("id").text,
-            "name": emp.find("name").text,
-            "role": emp.find("role").text
-        })
-
-    return employees
+if __name__ == "__main__":
+    main()
